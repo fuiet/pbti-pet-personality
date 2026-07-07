10a1cfd2dfac0fa05fee9b55251d96348ffeeeb2
fuiet <89821101+fuiet@users.noreply.github.com>
2026-07-07 16:03:01 +0800

    Render customer services dynamically in demo pages

--- demo/template/data-loader.js
+++ demo/template/data-loader.js
@@ -7,6 +7,16 @@ loadCustomer('../../customers/superior-landscapes.json')
 .then(data=>{
  const company=document.getElementById('company');
  const location=document.getElementById('location');
+ const services=document.getElementById('services');
+
  if(company) company.innerText=data.companyName;
  if(location) location.innerText=data.location;
+
+ if(services){
+  data.services.forEach(service=>{
+   const item=document.createElement('div');
+   item.innerHTML='<h3>'+service+'</h3><p>Professional solutions designed for your property.</p>';
+   services.appendChild(item);
+  });
+ }
 });
